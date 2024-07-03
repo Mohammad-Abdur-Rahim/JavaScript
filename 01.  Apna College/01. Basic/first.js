afde2056fa91321182d0f05278cbdce518eb6e6c
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-07-03 12:24:20 +0600

    *var let const Declare variable **

---
+++ 01.  Apna College/01. Basic/first.js
@@ -0,0 +1,7 @@
+// JavaScript  ----------- Variable and dataType ------------------------
+let name ="abdurRahime";
+let age =22;
+const universityName ="Daffodil International University";
+console.log(name);
+console.log(age);
+console.log(universityName);
