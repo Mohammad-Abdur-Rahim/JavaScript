ca4db3b6b39fc01f9a1b3306badfbfeea52f797f
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-07-03 19:32:15 +0600

    *Operator & Condition start **

--- 01.  Apna College/01. Basic/first.js
+++ 01.  Apna College/01. Basic/first.js
@@ -1,9 +1,2 @@
-// JavaScript  ----------- practice product card details = object create  ------------------------
-const product = {
-    name: "Parker Jotter Standard CT Ball Pen (Black)",
-    rating: 4,
-    isDeal: true,
-    price: 270,
-};
-console.log(product);
-console.log(typeof product.rating);
+// JavaScript  -----------  Operator & Condition use ...  ------------------------
+console.log("Start operator and condition");
