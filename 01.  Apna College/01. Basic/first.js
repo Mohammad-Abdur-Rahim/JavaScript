b453b1c5878cffa52f907b6c53466b1ce18b93d4
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-07-03 20:10:06 +0600

    *Comparison   operator a===b **

--- 01.  Apna College/01. Basic/first.js
+++ 01.  Apna College/01. Basic/first.js
@@ -2,10 +2,9 @@
 
 //comparison operator use
 let a=10;
-let b=10;
+let b="10";
 
 
-console.log("a==b",a==b);
-console.log("a===b",a===b);
-console.log("a!=b",a!=b);
-console.log("a!==b",a!==b);
+console.log("a==b",a==b);   //only value comparison
+console.log("a===b",a===b);  //value and data-type comparison
+
