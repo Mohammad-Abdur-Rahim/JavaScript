52c8ba0e3647be5254677b1772561c00d4227c88
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-07-10 20:27:22 +0600

    For Loop ****

--- 01.  Apna College/01. Basic/first.js
+++ 01.  Apna College/01. Basic/first.js
@@ -1,21 +1,4 @@
-// JavaScript  -----------  Operator & Condition use ...  ------------------------
-
-//Practice problem solve
-
-const score = 95;
-
-if(score>=80){
-    console.log("A")
-}
-else if(score>=70 && score<=79){
-    console.log("B")
-}
-else if(score>=60 && score<=69){
-    console.log("C")
-}
-else if(score>=50 && score<=59){
-    console.log("D")
-}
-else{
-    console.log("F")
+// JavaScript  -----------  Loops & String use ...  ------------------------
+for(let i=1;i<=5;i++){
+    console.log("{1}loop");
 }
