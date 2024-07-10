7490911d5fc5c03fbaab72ef686d67353629fe99
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-07-10 21:31:27 +0600

    *** Even & Odd Numbers implements ****  ..

--- 01.  Apna College/01. Basic/first.js
+++ 01.  Apna College/01. Basic/first.js
@@ -1,12 +1,8 @@
-// JavaScript  -----------  Loops & String use ...  ------------------------
-//For in loops use only (String,Array)****
-const student = {
-    name: "md",
-    id: 221,
-    age: 22,
-    cgpa: 3.19,
-    isPass: true,
-};
-for (let i in student){
-    console.log("key:",i,student[i]);
+//solve question print all even number 0 to 100;
+
+for(let i=0;i<=100;i++) {
+    // if(i%2!==0) odd formula
+    if(i%2===0){
+        console.log("Even numbers",i);
+    }
 }
