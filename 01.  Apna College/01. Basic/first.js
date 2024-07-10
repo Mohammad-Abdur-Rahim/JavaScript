28d0279f8609c331cd0f8e873f7da856b585b82d
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-07-10 22:02:28 +0600

    **String index & length ***

--- 01.  Apna College/01. Basic/first.js
+++ 01.  Apna College/01. Basic/first.js
@@ -1,8 +1,7 @@
-//create a game start with any random numbers.ask the user keep guessing the correct numbers until the game is finished   . 
-let gameNum = 25;
-let userNum = prompt("guessing the game number");
+// String representation now
 
-while (userNum != gameNum) {
-     userNum = prompt(" Again try");
-}
-console.log("Congratulation You win ");
+let str="Md Abdur Rahim";
+let str2='Md';
+console.log(str.length);
+console.log(str[9]);
+
