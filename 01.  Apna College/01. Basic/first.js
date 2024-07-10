ca0f3b40620ad10f06adc45f1b0fbd04b94925e6
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-07-10 20:59:16 +0600

    *** While loop sum of num,bers

--- 01.  Apna College/01. Basic/first.js
+++ 01.  Apna College/01. Basic/first.js
@@ -1,7 +1,9 @@
 // JavaScript  -----------  Loops & String use ...  ------------------------
 //While Loop
 let i=0;
+let sum=0
 while(i<=5){
-    console.log("i = " , i);
+    sum+=i;
     i++;
 }
+console.log("sum = " , sum);
