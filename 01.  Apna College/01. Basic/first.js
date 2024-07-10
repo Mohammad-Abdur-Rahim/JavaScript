32bdc40792a08c809421403f1aeab86fd6ff32f3
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-07-10 21:12:51 +0600

    *** For of Loop use find length  ..

--- 01.  Apna College/01. Basic/first.js
+++ 01.  Apna College/01. Basic/first.js
@@ -1,6 +1,9 @@
 // JavaScript  -----------  Loops & String use ...  ------------------------
 //For of loops use only (String,Array)****
 let str="JavaScript";
+let length=0;
 for(let i of str){
     console.log("i =",i);
+    length++;
 }
+console.log("length = ",length);
