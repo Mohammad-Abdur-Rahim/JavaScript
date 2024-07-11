ff4b3407a1cb5fb15940865974e587701fad1a51
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-07-11 09:58:29 +0600

    Escap character **

--- 01.  Apna College/01. Basic/first.js
+++ 01.  Apna College/01. Basic/first.js
@@ -1,8 +1,5 @@
 // Template String representation now
 
-let output={
-    item:"iphone xs",
-    price:20000,
-};
-console.log("phone:",output.item,"price",output.price);
-console.log(`item :${output.item} and price is ${output.price} taka`); //when use template str then number convert single ---single string-----
+
+console.log("Apna\ncollege");
+console.log("Apna\tcollege");
