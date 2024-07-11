d9dd98eab14d16f361ed0e59a849a16474351c09
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-07-11 11:50:19 +0600

    ** Username Create Solve Quyestion ***

--- 01.  Apna College/01. Basic/first.js
+++ 01.  Apna College/01. Basic/first.js
@@ -1,18 +1,13 @@
-//  String Method now
+//  String Question solve ------Generate username based user input------
 
-let str1='ApnaCollege';
-let str2='    Hello Programmer   ';  //trim remove first and last white space
-console.log(str1.toUpperCase());
-console.log(str1.toLowerCase());
-console.log(str2.trim());
+// let userName =prompt("Enter Your Full Name");
+
+// let userNameLength =userName.length;
 
 
-let string ="Hello Programmer";
-console.log(string.slice(0,7));  //slice start to end
+// console.log(`@${userName.concat(userNameLength)}`); 
+//My solve now apna college solve start
 
-let count1 ='Bangla';
-let count2 ='desh';
-console.log(count1.concat(count2)); // String Concatation
-console.log(count1.charAt(3)); // String Chart At(idx)
-
-
+let fullName = prompt("Enter Your Full Name");
+let userName = "@"+fullName+fullName.length;
+console.log(userName);
