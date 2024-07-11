2f4d17a24cf8e35058f795a825fc0c35463f1351
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-07-11 20:11:20 +0600

    * Array toString Method ** .. **

--- 01.  Apna College/01. Basic/first.js
+++ 01.  Apna College/01. Basic/first.js
@@ -1,7 +1,7 @@
 //  JavaScript ---- Array Methods  --------
 
-let numbers =[10,20,30,40,50,60,70,80,90,100,110,120];
-numbers.push(900);
-console.log(`After push :${numbers}`);   // push add new item last
-numbers.pop();
-console.log(`After Pop :${numbers}`);   // pop delete last value & return 
+let foodItems = ["apple", "banana","orange","tomato"];
+console.log(foodItems);
+console.log(foodItems.toString()); // convert array to string & Original not change array
+
+
