6a0dd98c2ea077426cb5723db4ffe450df25a450
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-07-11 18:20:48 +0600

    ** Array Find average  ** **

--- 01.  Apna College/01. Basic/first.js
+++ 01.  Apna College/01. Basic/first.js
@@ -1,7 +1,12 @@
 //  JavaScript ----Array ----
+//practice question 1-- array mark of student . find average mark of students
 
-let names = ["Rahim","Rakib","Rafin","Shafin","Alom"];
-
-for (let name of names) {
-    console.log(name.toUpperCase());
+let marks =[85,97,44,37,76,60];
+let len = marks.length
+let sum=0;
+for (mark of marks){
+sum += mark;
 }
+console.log("Sum of Array :",sum);
+let average = sum/len;
+console.log("Average of Array :",average);
