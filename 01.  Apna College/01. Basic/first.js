632b6cbc25b0ea14e420da3253d0b9c0af878d05
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-07-11 20:48:21 +0600

    Array Question solve ***

--- 01.  Apna College/01. Basic/first.js
+++ 01.  Apna College/01. Basic/first.js
@@ -1,9 +1,21 @@
 //  JavaScript ---- Array Methods  --------
+//practice problem array ----
+//1.remove first companies the array
+//2.remove Uber & Add Ola in Its Place
+//3. Add Amazon at the End
 
+let companies =["Bloomberg","Microsoft","uber","Google","IBM","netflix"];
+//ans 1-----------
+// console.log(companies);
+// console.log("Remove First companies :", companies.shift());
+// console.log(companies);
+//ans 2-----------
+// let newCompanies =companies.splice(2,1,"Ola");
+// console.log(companies);
+//ans 3-----------
+console.log(companies.push("Amazon"));
+console.log(companies);
 
-let num = [10, 20, 30, 40, 50,60,70,80];
-console.log(num);
-console.log(num.slice(0, 3));  //slice 0-2 only & Original array not change-------
 
 
 
