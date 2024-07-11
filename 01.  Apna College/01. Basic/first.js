9d7a48192846983a4b72af45b283f6c0955b88ea
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-07-11 10:15:41 +0600

    ** String Method**

--- 01.  Apna College/01. Basic/first.js
+++ 01.  Apna College/01. Basic/first.js
@@ -1,5 +1,9 @@
-// Template String representation now
+//  String Method now
+
+let str1='ApnaCollege';
+let str2='    Hello Programmer   ';  //trim remove first and last white space
+console.log(str1.toUpperCase());
+console.log(str1.toLowerCase());
+console.log(str2.trim());
 
 
-console.log("Apna\ncollege");
-console.log("Apna\tcollege");
