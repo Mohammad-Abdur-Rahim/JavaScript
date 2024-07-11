9d1577b038da4e595c2da9bd486941f92e000096
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-07-11 18:42:29 +0600

    ** Array Find 10% discount only for loop  ** **

--- 01.  Apna College/01. Basic/first.js
+++ 01.  Apna College/01. Basic/first.js
@@ -1,13 +1,19 @@
 //  JavaScript ----Array ----
 //practice question 2-- price of 5 items [250,645,300,900,50] all item 10% offer . change array to final price after apply offer....
 
+// let prices = [250,645,300,900,50];
+// let i=0;
+// for (let price of prices) {
+//     console.log(`value at index ${i} = ${price}`);
+//     //offer create
+//     let offer =price/10;
+//     prices[i]=prices[i]-offer;
+//     console.log(`value at index ${i} = ${prices[i]}`);
+//     i++;
+// }
 let prices = [250,645,300,900,50];
-let i=0;
-for (let price of prices) {
-    console.log(`value at index ${i} = ${price}`);
-    //offer create
-    let offer =price/10;
-    prices[i]=prices[i]-offer;
-    console.log(`value at index ${i} = ${prices[i]}`);
-    i++;
+for (let i=0;i<prices.length;i++) {
+    let offer = prices[i]/10;
+    prices[i]-=offer;
 }
+console.log(`value at index  = ${prices}`);
