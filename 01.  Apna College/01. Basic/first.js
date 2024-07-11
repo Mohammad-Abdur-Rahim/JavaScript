01277c3fc1a8dab5fdd97c4113532194142ff057
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-07-11 20:23:53 +0600

    * Array Shift/Unshift  Method ** .. **

--- 01.  Apna College/01. Basic/first.js
+++ 01.  Apna College/01. Basic/first.js
@@ -4,9 +4,9 @@
 let num = [10, 20, 30, 40, 50];
 num.unshift(440);  //unshift add number first place
 console.log(num);
-num.shift();   //shift delete number first place
+let test =num.shift();   //shift delete number first place
 console.log(num);
-
+//shift & Unshift Original value Change hoi
 
 
 
