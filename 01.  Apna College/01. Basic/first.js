9f05d8d3a7640586ea1e5427dfa08cf8cc0b5917
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-07-11 18:23:52 +0600

    ** Array Find average template string use  ** **

--- 01.  Apna College/01. Basic/first.js
+++ 01.  Apna College/01. Basic/first.js
@@ -2,11 +2,11 @@
 //practice question 1-- array mark of student . find average mark of students
 
 let marks =[85,97,44,37,76,60];
-let len = marks.length
+
 let sum=0;
 for (mark of marks){
 sum += mark;
 }
 console.log(`Sum of Array : ${sum}`);
-let average = sum/len;
+let average = sum/marks.length;
 console.log(`Average of Array : ${average}`);
