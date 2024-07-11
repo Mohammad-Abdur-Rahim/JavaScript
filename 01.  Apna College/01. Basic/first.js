9480deb68eb5bb852d91ed56dd8f752bfa17b859
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-07-11 18:38:32 +0600

    ** Array Find 10% discount  ** **

--- 01.  Apna College/01. Basic/first.js
+++ 01.  Apna College/01. Basic/first.js
@@ -1,12 +1,13 @@
 //  JavaScript ----Array ----
-//practice question 1-- array mark of student . find average mark of students
+//practice question 2-- price of 5 items [250,645,300,900,50] all item 10% offer . change array to final price after apply offer....
 
-let marks =[85,97,44,37,76,60];
-
-let sum=0;
-for (mark of marks){
-sum += mark;
+let prices = [250,645,300,900,50];
+let i=0;
+for (let price of prices) {
+    console.log(`value at index ${i} = ${price}`);
+    //offer create
+    let offer =price/10;
+    prices[i]=prices[i]-offer;
+    console.log(`value at index ${i} = ${prices[i]}`);
+    i++;
 }
-console.log(`Sum of Array : ${sum}`);
-let average = sum/marks.length;
-console.log(`Average of Array : ${average}`);
