85c08cd64d64a1bba9e644b5dbed824fd8f68014
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-07-11 09:50:04 +0600

    *** Template string use s

--- 01.  Apna College/01. Basic/first.js
+++ 01.  Apna College/01. Basic/first.js
@@ -4,4 +4,5 @@ let output={
     item:"iphone xs",
     price:20000,
 };
-console.log(`item :${output.item} and price is ${output.price} taka`);
+console.log("phone:",output.item,"price",output.price);
+console.log(`item :${output.item} and price is ${output.price} taka`); //when use template str then number convert single ---single string-----
