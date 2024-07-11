0dfa2002bb369eb2e3d1fabac445ca20f39fb577
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-07-11 10:22:36 +0600

    ** All  String Method**

--- 01.  Apna College/01. Basic/first.js
+++ 01.  Apna College/01. Basic/first.js
@@ -7,3 +7,12 @@ console.log(str1.toLowerCase());
 console.log(str2.trim());
 
 
+let string ="Hello Programmer";
+console.log(string.slice(0,7));  //slice start to end
+
+let count1 ='Bangla';
+let count2 ='desh';
+console.log(count1.concat(count2)); // String Concatation
+console.log(count1.charAt(3)); // String Chart At(idx)
+
+
