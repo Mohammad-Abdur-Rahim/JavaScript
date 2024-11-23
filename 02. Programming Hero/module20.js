79811d4f6922143f3e4c308e5f6215dda169cb9e
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-11-23 21:31:22 +0600

    Array looping index ***

--- 02. Programming Hero/module20.js
+++ 02. Programming Hero/module20.js
@@ -1,15 +1,16 @@
-// Object study ------------------------------
+const friends =['Elon','Bill','Mark','waren','Ambani'];
 
-const persons ={
-    name : "Md Abdur Rahim",
-    age : 23,
-    profession : 'Web Developer',
-    salary : 35000,
-    married : false
-};
-// loop access the object 
-for (const person in persons){
-    console.log(person);
-    console.log(persons[person]);
+for (const friend of friends){
+    console.log(friend);
 }
 
+
+// Advance index looping  logic programmers must know
+// for (let i=0; i<friends.length; i++) {
+//     // console.log([i]);
+//     console.log(friends[i]);
+// }
+const numbers =[1,2,3,4,5,6,7,8,9,10,11,12,13];
+for(let i=0; i<numbers.length; i++){
+    console.log(numbers[i]);
+}
