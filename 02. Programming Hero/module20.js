03f1b3fb8230883bf32a07ce5deb98818c81b0c5
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-11-23 21:51:39 +0600

    module 20 complete ***

--- 02. Programming Hero/module20.js
+++ 02. Programming Hero/module20.js
@@ -1,10 +1,8 @@
 //Reverse Array Without Function
+
+const numbers =[1,2,3,4,5,6,7,8,9,10,11,12,13];
 // numbers.reverse();
 
-const numbers =[1,2,3,4,5,6,7,8,9,10];
-const rev_num =[];
 for(const num of numbers){
-// console.log(num);
-rev_num.unshift(num);
+    console.log(num);
 }
-console.log(rev_num);
