0ab71d384ff9e1e0b8647a102eca0daebd8a26fc
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-11-23 19:12:31 +0600

    Breal/continue

--- 02. Programming Hero/module19.js
+++ 02. Programming Hero/module19.js
@@ -1,15 +1,9 @@
 
-// Even / ood number 
-// for(let i=1;i<20;i++){
-//     if(i%2===0){
-//         console.log(i);
-//     }
-// }
 
-
-for(let i=1;i<50;i++){
-    if(i%3===0 && i%5===0){
-        console.log(i);
+for(let i=1;i<20;i++){
+    console.log(i);
+    if(i>=10){
+        break;
     }
 }
 
