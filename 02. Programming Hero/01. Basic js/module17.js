bd4182a66a638c6f44917ad510c8599fc4c9d4df
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-11-23 14:35:33 +0600

    Module 17 complete

--- 02. Programming Hero/01. Basic js/module17.js
+++ 02. Programming Hero/01. Basic js/module17.js
@@ -1,25 +1,9 @@
-// String
-var country = "Bangladesh";
-console.log(country); 
-console.log(typeof country); 
-
-// Boolean 
-var isHappy =true;
-console.log(isHappy); 
-console.log(typeof isHappy); 
-
-// Number 
-var id = 5915;
-console.log(id); 
-console.log(typeof id); 
-// string to number convert 
-var orangePrice= 300;
-var applePrice = parseInt("2000");
-console.log(applePrice+orangePrice); 
-
-var first =0.1;
-var second =0.2;
-var total = first + second ;
-console.log(total.toFixed(1));
+// Problem 1 module 17 ---------
+var userInput = 1000;
+var applePrice = 200;
+var orangePrice = 200;
+var totalCost = applePrice + orangePrice;
+var remainingTaka=userInput - totalCost ;
+console.log(remainingTaka);
 
 
