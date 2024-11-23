75ca2c17a44e542bcf7662e7ef0712847c4db4ba
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-11-23 21:38:35 +0600

    Array reverse  ***

--- 02. Programming Hero/module20.js
+++ 02. Programming Hero/module20.js
@@ -1,16 +1,10 @@
-const friends =['Elon','Bill','Mark','waren','Ambani'];
+//Reverse Array Without Function
+// numbers.reverse();
 
-for (const friend of friends){
-    console.log(friend);
+const numbers =[1,2,3,4,5,6,7,8,9,10];
+const rev_num =[];
+for(const num of numbers){
+// console.log(num);
+rev_num.unshift(num);
 }
-
-
-// Advance index looping  logic programmers must know
-// for (let i=0; i<friends.length; i++) {
-//     // console.log([i]);
-//     console.log(friends[i]);
-// }
-const numbers =[1,2,3,4,5,6,7,8,9,10,11,12,13];
-for(let i=0; i<numbers.length; i++){
-    console.log(numbers[i]);
-}
+console.log(rev_num);
