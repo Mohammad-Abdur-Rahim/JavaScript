bb9587d659012b18196b4383fe22d4828a8cf5df
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-11-23 14:23:38 +0600

    module 17

---
+++ 02. Programming Hero/01. Basic js/module17.js
@@ -0,0 +1,25 @@
+// String
+var country = "Bangladesh";
+console.log(country); 
+console.log(typeof country); 
+
+// Boolean 
+var isHappy =true;
+console.log(isHappy); 
+console.log(typeof isHappy); 
+
+// Number 
+var id = 5915;
+console.log(id); 
+console.log(typeof id); 
+// string to number convert 
+var orangePrice= 300;
+var applePrice = parseInt("2000");
+console.log(applePrice+orangePrice); 
+
+var first =0.1;
+var second =0.2;
+var total = first + second ;
+console.log(total.toFixed(1));
+
+
