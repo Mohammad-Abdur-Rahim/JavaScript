af86f51bc7dd036478198a896825d2253d259996
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-11-23 19:56:55 +0600

    String lowercase/ trim() **

--- 02. Programming Hero/module20.js
+++ 02. Programming Hero/module20.js
@@ -1,1 +1,17 @@
 // String And Object Fundamental 
+const country ="Bangladesh is Our Country";
+
+console.log(country.toUpperCase());
+console.log(country.toLowerCase());
+
+
+const subject = "Math";
+const book = "math    ";
+
+if(subject.toLowerCase().trim()===book.toLowerCase().trim()){
+    console.log("Same");
+}
+else {
+    console.log("Not Same");
+
+}
