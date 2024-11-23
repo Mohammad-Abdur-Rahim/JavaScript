5a395f3dc4de94998249e9cbdc8086d24dcd95cb
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-11-23 17:04:17 +0600

    Array length

--- 02. Programming Hero/module18.js
+++ 02. Programming Hero/module18.js
@@ -1,15 +1,5 @@
-// Calculate BMI 
-const BMI =55 ;
-
-if(BMI <18){
-    console.log("UnderWeight");
-}
-else if (BMI >=18.5 && BMI <= 24.9){
-    console.log("Normal");
-}
-else if (BMI >= 25 && BMI <= 29.9){
-    console.log("OverWeight");
-}
-else{
-    console.log("Obese")
-}
+// Array part 1 
+const numbers = [1, 2, 3, 4, 5];
+const friend =['rahim','rakib','tahsin'];
+console.log(numbers.length);
+console.log(friend.length);
