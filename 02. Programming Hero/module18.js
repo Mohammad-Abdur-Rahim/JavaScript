0d1a3660a33d6c26b9d99fc908c7e94da59d4dee
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-11-23 17:08:59 +0600

    Array Index

--- 02. Programming Hero/module18.js
+++ 02. Programming Hero/module18.js
@@ -1,5 +1,10 @@
 // Array part 1 
 const numbers = [1, 2, 3, 4, 5];
-const friend =['rahim','rakib','tahsin'];
-console.log(numbers.length);
-console.log(friend.length);
+console.log(numbers[0]);
+
+const fourth = numbers[4];
+console.log(fourth);
+
+// set or update 
+numbers[4] = 999;
+console.log(numbers);
