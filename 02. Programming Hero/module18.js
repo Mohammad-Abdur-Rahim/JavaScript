9525af4f06f583dd05402f2150eafae86721fc72
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-11-23 16:05:11 +0600

    if else basic

---
+++ 02. Programming Hero/module18.js
@@ -0,0 +1,9 @@
+// Fundamental conditional operation if else 
+const salary = 45000;
+const webDeveloper = true;
+if(salary > 35000 && webDeveloper==true) {
+    console.log("confirm i will join your company ");
+}
+else {
+    console.log("sorry your salary level is low ");
+}
