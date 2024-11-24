f98f919560dd318f778205148eaaa2757161d10e
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-11-24 20:01:18 +0600

    Feet to Inche Convert **

--- 02. Programming Hero/module22.js
+++ 02. Programming Hero/module22.js
@@ -1,1 +1,25 @@
 // JavaScript simple coding problem 
+
+// Feet To Inch Formula 
+
+function feetToInch(inch){
+    const feet = parseInt( inch / 12) ;
+    const inchRemaining = inch % 12 ;
+    const result = feet + "Feet" + inchRemaining + "Inch" ;
+    return result;
+
+};
+const inch = 125;
+const output = feetToInch(inch);
+console.log(output);
+
+// easy method -------------------------------
+// function feetToInch(inch){
+//     const feet = inch /12 ;
+//     return feet.toFixed(2);
+// };
+// const inch = 125;
+// const output = feetToInch(inch);
+// console.log(output);
+
+
