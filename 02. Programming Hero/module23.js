548a6b3a417d2499dbf73a574a40d550caaf0af1
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-11-24 22:07:25 +0600

    wood qunantity function  **

--- 02. Programming Hero/module23.js
+++ 02. Programming Hero/module23.js
@@ -1,22 +1,16 @@
+function woodQuantity(chair,table,bed){
+    perChair = 3;
+    perTable = 10;
+    perBed = 60;
+    const chairTotalWood = chair * perChair ;
+    const tableTotalWood = table * perTable ;
+    const bedTotalWood = bed * perBed ;
+    const totalWood = chairTotalWood + tableTotalWood + bedTotalWood;
+    return totalWood;
 
-
-function maxNumber(num1, num2, num3) {
-    if(num1>num2 && num1>num3){
-        return "Num 1 is Big";
-    
-    }
-    else if(num2>num1 && num2>num3){
-        return "Num 2 is Big";
-    
-    
-    }
-    else{
-      return "Num 3 is Big";
-    
-    }
 }
-const num1 =10;
-const num2 =90;
-const num3 =890;
-const output = maxNumber(num1, num2, num3);
-console.log(output);
+const chair = 5;
+const table = 3;
+const bed = 2;
+const output = woodQuantity(chair,table,bed);
+console.log("Total Wood :",output);
