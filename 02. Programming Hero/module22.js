f5245ad5223f47afeffce409d18fff9a1e80db64
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-11-24 20:42:49 +0600

    Duplicate array FN  **

--- 02. Programming Hero/module22.js
+++ 02. Programming Hero/module22.js
@@ -1,19 +1,17 @@
-// Calculate the average number of array odd number length
-
-function arrayOddAverage(numberOfArray) {
-    const oddNumber = [];
-    for (const number of numberOfArray) {
-        if (number % 2 === 1) {
-            oddNumber.push(number);
+// Remove duplicate value array 
+function duplicateCheck(array) {
+    const uniqueArray = [];
+    for (const number of array) {
+        if (uniqueArray.includes(number) === false) {
+            uniqueArray.push(number);
         }
     }
-    // console.log(oddNumber);
-    let sum =0;
-    for(const number of oddNumber) {
-        sum += number;
-    }
-    return sum/oddNumber.length;  
-}
-const numbers = [22, 33, 45, 356, 566, 33, 55, 51];
-const output = arrayOddAverage(numbers);
-console.log("Average Odd numbers is :", output);
+    return uniqueArray;
+
+};
+
+
+
+const numbers = [22, 33, 45, 566, 45, 50, 50, 356, 566, 33, 55, 51];
+const output = duplicateCheck(numbers);
+console.log("Check duplicate  :", output);
