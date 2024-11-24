1c5e121302206ed2e9ccc038d3bbac87979d6825
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-11-24 21:07:54 +0600

    Swap 2 value Destrucing  **

--- 02. Programming Hero/module22.js
+++ 02. Programming Hero/module22.js
@@ -1,8 +1,6 @@
 let num1 = 10;
 let num2 = 900;
 
-num1 = num1 + num2; 
-num2 = num1 - num2; 
-num1 = num1 - num2; 
+[num1, num2] = [num2, num1]; 
 
-console.log(num1, num2); 
+console.log(num1, num2);
