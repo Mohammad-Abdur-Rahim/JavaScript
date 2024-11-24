5b66a20fd293d0eb5da49f354fe5f69b0ab6c3bc
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-11-24 20:12:16 +0600

    Leaf Year fn 2 **

--- 02. Programming Hero/module22.js
+++ 02. Programming Hero/module22.js
@@ -1,12 +1,17 @@
-function leafYear(year) {
-    if (year % 4 === 0) {
-      return 'leaf Year';
+
+
+function isLeapYear(year) {
+   
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+      return 'Leap Year';
     } else {
-      return 'Not leaf Year';
+      return 'Not Leap Year';
     }
   }
   
-  const year = 2001;
-  const output = leafYear(year);
-  console.log(output); // Prints "Not leaf Year"
+  // Test cases
+  console.log(isLeapYear(2000)); 
+  console.log(isLeapYear(1900)); 
+  console.log(isLeapYear(2004));
+  console.log(isLeapYear(2001)); 
   
