71cf80a19e5e22ab7584694f9ae0aa55eeb713b3
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-11-24 21:06:56 +0600

    Swap 2 value without variable  **

--- 02. Programming Hero/module22.js
+++ 02. Programming Hero/module22.js
@@ -1,9 +1,8 @@
-//SWap two variable 
 let num1 = 10;
 let num2 = 900;
 
-let temp = num1; 
-num1 = num2;   
-num2 = temp;   
+num1 = num1 + num2; 
+num2 = num1 - num2; 
+num1 = num1 - num2; 
 
 console.log(num1, num2); 
