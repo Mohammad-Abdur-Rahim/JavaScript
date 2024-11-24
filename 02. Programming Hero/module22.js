0a764c5690fa5f6a2b49199dd3a0547ecd63262e
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-11-24 20:56:27 +0600

    Math ,abs,round,floor,ceil,random Fn  **

--- 02. Programming Hero/module22.js
+++ 02. Programming Hero/module22.js
@@ -1,17 +1,14 @@
-// Remove duplicate value array 
-function duplicateCheck(array) {
-    const uniqueArray = [];
-    for (const number of array) {
-        if (uniqueArray.includes(number) === false) {
-            uniqueArray.push(number);
-        }
-    }
-    return uniqueArray;
+const min = Math.min(10,20,30);
+console.log(min);
+const max = Math.max(10,20,30);
+console.log(max);
 
-};
+console.log(Math.PI);
+console.log(Math.abs(-99));
+console.log(Math.round(2.5));
+console.log(Math.floor(2.25));
+console.log(Math.ceil(2.25));
 
+//Math Random
+console.log(Math.random());
 
-
-const numbers = [22, 33, 45, 566, 45, 50, 50, 356, 566, 33, 55, 51];
-const output = duplicateCheck(numbers);
-console.log("Check duplicate  :", output);
