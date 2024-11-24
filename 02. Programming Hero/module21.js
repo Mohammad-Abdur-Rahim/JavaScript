696ab0a37bdd4f100b05f0a34d7ea03d72b1a079
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-11-24 11:32:56 +0600

    Sum & Suare Function ***

--- 02. Programming Hero/module21.js
+++ 02. Programming Hero/module21.js
@@ -1,5 +1,16 @@
 // Function javaScript implements 
-function person(){
-    console.log("This person function implements");
+
+// Add function 
+function sum (num1,num2){
+    const sum = num1 + num2;
+console.log(sum)
 }
-person();
+sum(2,8);
+
+// square function 
+
+function person(number){
+    const square = number*number;
+    console.log(square);
+}
+person(12);
