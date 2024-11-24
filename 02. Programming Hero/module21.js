801a71ca8ffc732596c95d64d516d9fc799eb15a
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-11-24 12:38:10 +0600

    Array sum Function**

--- 02. Programming Hero/module21.js
+++ 02. Programming Hero/module21.js
@@ -1,15 +1,23 @@
-function evenString(str) {
-    const len = str.length;
-    console.log(len)
-    if(len % 2 === 0){
-        console.log('even size');
-        return true;
-    }
-    else {
-        console.log('Odd size');
+function sumArrays(arrays){
 
-        return false;
-    }
+    let sum =0;
+for(const array of arrays){
+console.log(array);
+ sum = sum + array;
 }
-evenString("rahim");
-evenString("Adil");
+return sum;
+
+}
+
+const num = [10,20,30,40,50];
+const sumArray = sumArrays(num);
+console.log("sum of numbers :",sumArray);
+
+
+// const numArray = [10,20,30,40,50];
+// let sum=0;
+// for(const array of numArray){
+//     console.log("sum of numbers :",array);
+//     sum+=array ;
+// }
+// console.log(sum);
