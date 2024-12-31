433cdf8a625ddcb3e85c57c74b3a23a607d8b359
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-12-31 21:55:17 +0600

    Style and class style add

--- 02. Programming Hero/02. Dom js/Milestone_5 (25-29)/js/dom.js
+++ 02. Programming Hero/02. Dom js/Milestone_5 (25-29)/js/dom.js
@@ -1,2 +1,16 @@
-const title = document.getElementById('skill').style.backgroundColor='yellow';
+const section = document.querySelectorAll('section');
+// console.log(section);
+for (const sec of section){
 
+sec.style.border = '2px solid black';
+sec.style.borderRadius = '5px';
+sec.style.padding = '10px';
+sec.style.backgroundColor='lightgray'
+
+}
+
+// const skillContainer =document.getElementById('skill-Container');
+// skillContainer.style.backgroundColor='yellow';
+
+const skillContainer =document.getElementById('skill-Container');
+skillContainer.classList.add('text-center');
