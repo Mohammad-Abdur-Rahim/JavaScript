95eb068f452bd43e4db91d5e62b8eea163628886
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-12-31 21:29:20 +0600

    Style Add

--- 02. Programming Hero/02. Dom js/Milestone_5 (25-29)/js/dom.js
+++ 02. Programming Hero/02. Dom js/Milestone_5 (25-29)/js/dom.js
@@ -1,6 +1,2 @@
-//Querey Selector
-const someLi = document.querySelectorAll('.ielts_Container li');
-//Dom Travers System
-for (const li of someLi) {
-    console.log(li);
-}
+const title = document.getElementById('skill').style.backgroundColor='yellow';
+
