aa8c4de69e9b80284baeb4177e9cd3b96cba411c
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-12-31 22:12:29 +0600

    Create Element add

--- 02. Programming Hero/02. Dom js/Milestone_5 (25-29)/js/dom.js
+++ 02. Programming Hero/02. Dom js/Milestone_5 (25-29)/js/dom.js
@@ -1,16 +1,4 @@
-const section = document.querySelectorAll('section');
-// console.log(section);
-for (const sec of section){
-
-sec.style.border = '2px solid black';
-sec.style.borderRadius = '5px';
-sec.style.padding = '10px';
-sec.style.backgroundColor='lightgray'
-
-}
-
-// const skillContainer =document.getElementById('skill-Container');
-// skillContainer.style.backgroundColor='yellow';
-
-const skillContainer =document.getElementById('skill-Container');
-skillContainer.classList.add('text-center');
+const addList = document.getElementById('addList');
+const li = document.createElement('li');
+li.innerText = 'Node-Js';
+addList.appendChild(li)
