a9adf16cc71e92527a3bedf877c0517693842830
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-01-02 16:08:30 +0600

    *** OnClick/addEventListener ****

--- 02. Programming Hero/02. Dom js/Milestone_5 (25-29)/js/dom.js
+++ 02. Programming Hero/02. Dom js/Milestone_5 (25-29)/js/dom.js
@@ -1,5 +1,24 @@
-const addList = document.getElementById('addList');
-const li = document.createElement('li');
-li.innerText = 'Node-Js';
-addList.appendChild(li)
-//Same as container h1 all tags
+// ------------ most case use 1st ---------------
+function makeRed(){
+    document.body.style.backgroundColor ='red';
+}
+// ---- 2nd
+const blue_button = document.getElementById('blue_button');
+blue_button.onclick = makeBlue;
+function makeBlue(){
+    document.body.style.backgroundColor ='Blue';
+}
+// ---- 3rd
+const orange_button = document.getElementById('orange_button');
+orange_button.onclick = function bg_orange(){
+    document.body.style.backgroundColor ='orange';
+}
+// ---- 4th
+const pink_button = document.getElementById('pink_button');
+pink_button.addEventListener('click',function bg_pink(){
+    document.body.style.backgroundColor ='pink';
+})
+// ---- 5th  important *******
+document.getElementById('black_button').addEventListener('click',function (){
+    document.body.style.backgroundColor ='black';
+})
