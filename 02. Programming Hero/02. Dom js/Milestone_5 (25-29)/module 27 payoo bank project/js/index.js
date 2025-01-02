22fd90724ccd103b5a4b480714b2c8bc2b32b11b
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-01-02 20:53:29 +0600

    ****  user login data check (Validation) ***

--- 02. Programming Hero/02. Dom js/Milestone_5 (25-29)/module 27 payoo bank project/js/index.js
+++ 02. Programming Hero/02. Dom js/Milestone_5 (25-29)/module 27 payoo bank project/js/index.js
@@ -1,1 +1,20 @@
-console.log('Running');
+// step:1 set event handler
+document.getElementById('login_btn').addEventListener('click', function (event) {
+    // step:2 prevent default (reloading Browser)
+    event.preventDefault(); 
+    console.log('Login Clicked with input data');
+    // step:3 get the user phone Number
+    const PhoneNumber =document.getElementById('input_number').value;
+    const PinNumber =document.getElementById('input_pin').value;
+    // console.log(PhoneNumber,PinNumber)
+    
+    //step:4  validation User Data
+    //Temporary usecase login page not use in -- future this way ()------
+    if(PhoneNumber==='01995516880' && PinNumber==='36990'){
+        console.log('You Are Login Successfully------');
+    }
+    else{
+        alert('Wrong User Data. Try Again ---');
+    }
+    
+});
