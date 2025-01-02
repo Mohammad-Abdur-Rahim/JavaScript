4cb8441bf2c3dd8c879d1b7377e6c0cc3cac9c9f
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-01-02 16:33:51 +0600

    *** input value text update ****

--- 02. Programming Hero/02. Dom js/Milestone_5 (25-29)/js/dom.js
+++ 02. Programming Hero/02. Dom js/Milestone_5 (25-29)/js/dom.js
@@ -1,24 +1,11 @@
-// ------------ most case use 1st ---------------
-function makeRed(){
-    document.body.style.backgroundColor ='red';
-}
-// ---- 2nd
-const blue_button = document.getElementById('blue_button');
-blue_button.onclick = makeBlue;
-function makeBlue(){
-    document.body.style.backgroundColor ='Blue';
-}
-// ---- 3rd
-const orange_button = document.getElementById('orange_button');
-orange_button.onclick = function bg_orange(){
-    document.body.style.backgroundColor ='orange';
-}
-// ---- 4th
-const pink_button = document.getElementById('pink_button');
-pink_button.addEventListener('click',function bg_pink(){
-    document.body.style.backgroundColor ='pink';
-})
-// ---- 5th  important *******
-document.getElementById('black_button').addEventListener('click',function (){
-    document.body.style.backgroundColor ='black';
+document.getElementById('update').addEventListener('click', function(){
+    const input_Text = document.getElementById('input_Text')
+    const input_value = input_Text.value;
+
+    const defaultChange = document.getElementById('default');
+    defaultChange.innerText = input_value;
+    // important  to clear input field text
+    input_Text.value='';
+
+    
 })
