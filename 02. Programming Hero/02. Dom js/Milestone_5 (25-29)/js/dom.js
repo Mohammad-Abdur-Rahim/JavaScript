a9f052db52e994d5e1dc255b78fb4bce9f6a39da
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-01-02 19:30:11 +0600

    Module 26 complete

--- 02. Programming Hero/02. Dom js/Milestone_5 (25-29)/js/dom.js
+++ 02. Programming Hero/02. Dom js/Milestone_5 (25-29)/js/dom.js
@@ -1,16 +1,16 @@
 
-document.getElementById('ul_Container').addEventListener('click', function(){
+document.getElementById('ul_Container').addEventListener('click', function () {
     console.log('Ul clicked')
 });
-document.getElementById('sec_container').addEventListener('click', function(){
+document.getElementById('sec_container').addEventListener('click', function () {
     console.log('Section clicked')
 });
 
-document.getElementById('two').addEventListener('click', function(event){
-   event.stopPropagation();
-   console.log('list clicked 1')
-}); 
-document.getElementById('two').addEventListener('click', function(event){
-  
+document.getElementById('two').addEventListener('click', function (event) {
+    event.stopPropagation();
+    console.log('list clicked 1')
+});
+document.getElementById('two').addEventListener('click', function (event) {
+
     console.log('list clicked 2')
- });
+});
