4aa0e037dd5feb6c32de28249f2f071cd00938bf
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-01-03 14:58:15 +0600

    Function basic

---
+++ 02. Programming Hero/02. Dom js/Milestone_5 (25-29)/payoo bank project - Function()/addmoney.js
@@ -0,0 +1,7 @@
+document.getElementById('addMoneyBtn').addEventListener('click',function (e){
+    e.preventDefault();
+    // const addMoney= document.getElementById('amountNumber').value
+    // const pinNumber= document.getElementById('pinNumber').value
+    const addMoney = getInputValue();
+    const pinNumber = getInputPin();
+});
---
+++ 02. Programming Hero/02. Dom js/Milestone_5 (25-29)/payoo bank project - Function()/utilities.js
@@ -0,0 +1,8 @@
+function getInputValue(){
+        const addMoney= document.getElementById('amountNumber').value
+        return addMoney;
+}
+function getInputPin(){
+    const pinNumber= document.getElementById('pinNumber').value
+    return pinNumber;
+}
