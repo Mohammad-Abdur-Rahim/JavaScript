909c15a5e61ee4c23ef03eed91295f608c7345a4
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-01-05 21:11:24 +0600

    *** FN default parameter set ***

--- 02. Programming Hero/03. Api/milestone_06 module(30-36)/js/index.js
+++ 02. Programming Hero/03. Api/milestone_06 module(30-36)/js/index.js
@@ -1,1 +1,14 @@
-console.log('Setup done')
+function add(n1=0,n2=0){
+    const result =n1+n2;
+    return result;
+
+};
+// ARRAY DEFAULT VALUE = [] 
+function fullname(fname='' , lname=''){
+    const result = fname+lname;
+    return result;
+}
+const output1 =add(5);
+const output3 =fullname('Md Abdur Rahim');
+console.log(output1);
+console.log(output3);
