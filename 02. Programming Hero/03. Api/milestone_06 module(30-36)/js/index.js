a9b479fedf7fa0b0dd1a78404af1e329b8b10fc7
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-01-05 21:53:37 +0600

    SPREAD (...ARRAY)

--- 02. Programming Hero/03. Api/milestone_06 module(30-36)/js/index.js
+++ 02. Programming Hero/03. Api/milestone_06 module(30-36)/js/index.js
@@ -1,5 +1,13 @@
-// Arrow Function 3 way declare parameter
-const first = ()=> console.log('first');
-const second = (a,b)=> a+b;
-const third = (a,b,c)=> a*b*c;
+// Spread js
+const max =Math.max(10,20,11,22,33,44,99);
+const number =[11,23,211,45,76,99];
+const arrayMax =Math.max(...number)
+console.log(max);
+console.log(arrayMax);
+// use spread operator to copy array value :
 
+const n1=[1,2,3,4,5];
+const n2 = [...n1];
+n2.push(99);
+console.log(n1);
+console.log(n2);
