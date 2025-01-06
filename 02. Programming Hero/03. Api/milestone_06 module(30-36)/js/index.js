200dd625720412d1f7e3ebda8f528f4c2a2d3cf0
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-01-06 20:05:08 +0600

    ** forEach use case

--- 02. Programming Hero/03. Api/milestone_06 module(30-36)/js/index.js
+++ 02. Programming Hero/03. Api/milestone_06 module(30-36)/js/index.js
@@ -1,16 +1,4 @@
-// Map ==> Methods  ----------
+// ForEach ==> Methods  ----------
 const number =[10,20,3,44,55];
-
-
-// const  doubleIt =(n)=> n*2;
-
-// const output = number.map(doubleIt);
-const output = number.map((n)=> n*2);
-console.log(output);
-
-const friends =['rakib','roni','asif','munna'];
-// for(const f of friends){
-//     console.log(f.length)
-// }
-const length = friends.map(friends=>friends.length);
-console.log(length);
+const result = number.forEach(n=>console.log(n));
+console.log(result);
