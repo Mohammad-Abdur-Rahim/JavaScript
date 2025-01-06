3f5fd1d3c5f4079a77d0fb2932e5307956a51bb5
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-01-06 20:01:24 +0600

    ** map use case

--- 02. Programming Hero/03. Api/milestone_06 module(30-36)/js/index.js
+++ 02. Programming Hero/03. Api/milestone_06 module(30-36)/js/index.js
@@ -1,9 +1,16 @@
 // Map ==> Methods  ----------
 const number =[10,20,3,44,55];
-function doubleIt(num){
-    console.log(num);
-    return num*2;
-}
 
-const output = number.map(doubleIt);
+
+// const  doubleIt =(n)=> n*2;
+
+// const output = number.map(doubleIt);
+const output = number.map((n)=> n*2);
 console.log(output);
+
+const friends =['rakib','roni','asif','munna'];
+// for(const f of friends){
+//     console.log(f.length)
+// }
+const length = friends.map(friends=>friends.length);
+console.log(length);
