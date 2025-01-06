437a829bc839cb4d720595c29d4045369786fe20
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-01-06 23:18:23 +0600

    Function argument

--- 02. Programming Hero/03. Api/milestone_06 module(30-36)/js/index.js
+++ 02. Programming Hero/03. Api/milestone_06 module(30-36)/js/index.js
@@ -1,4 +1,10 @@
-// Complete Module 31
+// Complete Module 32
 
+function sum(a,b,c){
+    // console.log(arguments); 
+    return a + b + c;
+}
+const output = sum(10,20,30,40,66,60);
+console.log(output);
 
 
