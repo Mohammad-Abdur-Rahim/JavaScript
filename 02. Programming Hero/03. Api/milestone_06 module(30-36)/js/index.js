bc36c3fe02cdf1daa4e8e301b92f77be7e3a938e
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-01-06 20:36:03 +0600

    ** Object to   Reduce()

--- 02. Programming Hero/03. Api/milestone_06 module(30-36)/js/index.js
+++ 02. Programming Hero/03. Api/milestone_06 module(30-36)/js/index.js
@@ -4,11 +4,11 @@ const product =[
     {id:2, name:'hp', price:82000, color:'black'},
     {id:3, name:'mac', price:172000, color:'silver'},
     {id:4, name:'lenovo', price:52000, color:'red'},
-    {id:5, name:'walton', price:42000, color:'silver'},
+    {id:5, name:'walton', price:12000, color:'silver'},
 ];
 
-
-const price =product.find((product)=>product.price >= 60000);
+//reduce find total object price
+const price =product.reduce((p,c)=>p+c.price ,0)
 console.log(price);
 
 
