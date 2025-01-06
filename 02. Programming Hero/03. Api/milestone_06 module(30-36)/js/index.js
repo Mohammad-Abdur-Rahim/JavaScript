8ebfa0d8f510193cb340d549231d576f7947922d
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-01-06 21:21:27 +0600

    Module 31  done

--- 02. Programming Hero/03. Api/milestone_06 module(30-36)/js/index.js
+++ 02. Programming Hero/03. Api/milestone_06 module(30-36)/js/index.js
@@ -1,20 +1,4 @@
-// class declaration in js 
+// Complete Module 31
 
-class Vehicle {
-    constructor(name,price){
-        this.name = name;
-        this.price = price;
-    }
-    move(){
-        console.log("Move To Finland Inshallah");
-    }
-}
-class Bus extends Vehicle {
-    constructor(name,price,seat,ticket){
-        super(name,price);
-        this.seat = seat;
-        this.ticket = ticket;
-    }
-}
 
 
