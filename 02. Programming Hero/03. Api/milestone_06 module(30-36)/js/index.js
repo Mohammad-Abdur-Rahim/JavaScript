e838f85b46bbff5915f9bd110baddb32f8bf60a3
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-01-06 20:20:58 +0600

    ** Reduce  use case

--- 02. Programming Hero/03. Api/milestone_06 module(30-36)/js/index.js
+++ 02. Programming Hero/03. Api/milestone_06 module(30-36)/js/index.js
@@ -1,6 +1,8 @@
-// filter ==> Methods  ----------
+// reduce ==> Methods  ----------
 const number =[10,20,3,44,55];
 
 
-const selected = number.find( n=> n%2===0 );
+const selected = number.reduce((previous,current)=>previous+current,0)
 console.log(selected);
+
+
