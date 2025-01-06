c9d78489b763c7486fe96f0d64c7e6a208db0e62
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-01-06 23:38:36 +0600

    Module 32 done

--- 02. Programming Hero/03. Api/milestone_06 module(30-36)/js/index.js
+++ 02. Programming Hero/03. Api/milestone_06 module(30-36)/js/index.js
@@ -1,10 +1,3 @@
-// Complete Module 32
-
-function sum(a,b,c){
-    // console.log(arguments); 
-    return a + b + c;
-}
-const output = sum(10,20,30,40,66,60);
-console.log(output);
+// Complete Module 32 end sum of important mind knowledge
 
 
