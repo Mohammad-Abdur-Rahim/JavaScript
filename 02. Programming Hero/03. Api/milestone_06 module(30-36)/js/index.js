306baf1011ea57fd799b7e1d829915a81332293b
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-01-06 20:50:53 +0600

    **Class create js

--- 02. Programming Hero/03. Api/milestone_06 module(30-36)/js/index.js
+++ 02. Programming Hero/03. Api/milestone_06 module(30-36)/js/index.js
@@ -1,16 +1,15 @@
-//Array of Object ---------- method use case
-const product =[
-    {id:1, name:'dell', price:72000, color:'silver'},
-    {id:2, name:'hp', price:82000, color:'black'},
-    {id:3, name:'mac', price:172000, color:'silver'},
-    {id:4, name:'lenovo', price:52000, color:'red'},
-    {id:5, name:'walton', price:12000, color:'silver'},
-];
+// class declaration in js 
 
-//reduce find total object price
-const price =product.reduce((p,c)=>p+c.price ,0)
-console.log(price);
-
-
+class Teacher{
+    constructor(name,subject){
+        this.name =name;
+        this.subject=subject;
+    }
+    lecture(){
+        console.log('teaching is math');
+    }
+}
+const Aliza =new Teacher('Aliza','Operating System');
+console.log(Aliza);
 
 
