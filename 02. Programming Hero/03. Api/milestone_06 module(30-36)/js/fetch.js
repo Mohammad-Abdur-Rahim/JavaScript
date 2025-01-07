27aa3852b17ccf8779c3a88fc7c56ef5f0754c09
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-01-07 12:19:05 +0600

    *** data load for of ***

--- 02. Programming Hero/03. Api/milestone_06 module(30-36)/js/fetch.js
+++ 02. Programming Hero/03. Api/milestone_06 module(30-36)/js/fetch.js
@@ -1,18 +1,15 @@
-// fetch('https://jsonplaceholder.typicode.com/todos/1')
-//       .then(response => response.json())
-//       .then(json => console.log(json))
 
-
-function function1(){
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then(response=> response.json())
-    .then(data => console.log(data))
-}
 function users(){
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(res => res.json())
     .then(data => displayUser(data))
 }
 function displayUser(data){
-    console.log(data)
+   const ul =document.getElementById('uesrList');
+   for(const users of data){
+    console.log(users);
+    const li = document.createElement('li');
+    li.innerText = users.name;
+    ul.appendChild(li);
+   }
 }
