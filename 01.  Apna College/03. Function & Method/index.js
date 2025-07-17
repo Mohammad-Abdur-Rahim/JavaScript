fc206d19d5ed72b69e450447c259cd158896a3bf
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-07-17 23:19:09 +0600

    Arrow Fn Vowel Count

--- 01.  Apna College/03. Function & Method/index.js
+++ 01.  Apna College/03. Function & Method/index.js
@@ -1,5 +1,36 @@
-// Arrow Function 
-const arrowSum =(a,b)=>{
-console.log(a+b);
+// function countVowel(name) {
+//   let count = 0;
+//   for (let data of name) {
+//     if (
+//       data === "a" ||
+//       data === "e" ||
+//       data === "i" ||
+//       data === "o" ||
+//       data === "u"
+//     ) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+
+// const output = countVowel("rahim");
+// console.log(`Vowel Number Is : ${output}`);
+
+const countVowel=(str)=>{
+   let count = 0;
+  for (let data of str) {
+    if (
+      data === "a" ||
+      data === "e" ||
+      data === "i" ||
+      data === "o" ||
+      data === "u"
+    ) {
+      count++;
+    }
+  }
+  return count;
 }
-arrowSum(10,20);
+const output = countVowel("rahim");
+console.log(`Vowel Number Is : ${output}`);
