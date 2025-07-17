c3f7ddada376ac2afa0c57adc362f59a563995c3
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-07-17 20:56:46 +0600

    Array print basic/for of

--- 01.  Apna College/02. Array & method/index.js
+++ 01.  Apna College/02. Array & method/index.js
@@ -1,1 +1,9 @@
-console.log("Done");
+let marks = [1,33,334,55,67,8,9];
+for(let i=0; i<marks.length;i++){
+console.log( marks[i]);
+}
+let names = ["rahim","Rony","Rakib","asif"];
+
+for(let name of names){
+    console.log( name);
+}
