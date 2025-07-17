6df83c58ae9a977565331fa57d605aad9bee0ae7
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-07-17 23:55:52 +0600

    Filter even

--- 01.  Apna College/03. Function & Method/index.js
+++ 01.  Apna College/03. Function & Method/index.js
@@ -1,11 +1,8 @@
-// let arr = [1, 2, 3, 4, 5];
-// arr.forEach((data)=>{
-//   console.log(data*data)
-// })
+// filter Return The Value 
 
-let arr = [1, 2, 3, 4, 5];
+let arr = [1, 2, 3, 4, 5,6,77,43,90];
 
-const square =(num)=>{
-  console.log(num*num);
-};
-arr.forEach(square);
+let evenArray = arr.filter((data)=>{
+    return data%2===0;
+});
+console.log(evenArray);
