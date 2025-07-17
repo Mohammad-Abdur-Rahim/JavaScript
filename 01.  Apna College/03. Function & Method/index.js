2bdc35f839bd6a4c7b195e41764e60ee589d8c69
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-07-17 23:39:56 +0600

    ForEach item multiply

--- 01.  Apna College/03. Function & Method/index.js
+++ 01.  Apna College/03. Function & Method/index.js
@@ -1,5 +1,11 @@
+// let arr = [1, 2, 3, 4, 5];
+// arr.forEach((data)=>{
+//   console.log(data*data)
+// })
+
 let arr = [1, 2, 3, 4, 5];
-arr.forEach((data, idx, arr) => {
-  // include 3 value forEach 
-  console.log(data, idx, arr);
-});
+
+const square =(num)=>{
+  console.log(num*num);
+};
+arr.forEach(square);
