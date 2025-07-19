0916cc667913d76fc8b87b3d50688555b3ce2a2c
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-07-19 20:58:32 +0600

    ** Window Object Console ***

--- 01.  Apna College/04. Dom/index.js
+++ 01.  Apna College/04. Dom/index.js
@@ -1,1 +1,6 @@
-console.log("test");
+// console.log("test");
+console.dir(window);
+console.dir(window.document);
+console.dir(document.body);
+console.dir(document.head);
+console.dir(document.body.childNodes[1]);
