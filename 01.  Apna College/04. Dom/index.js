ac545fbccba813f305345d3bcc474bf91cfe0871
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-07-25 00:45:07 +0600

    get/set Attrubutes basic

--- 01.  Apna College/04. Dom/index.js
+++ 01.  Apna College/04. Dom/index.js
@@ -1,10 +1,4 @@
-// Qustion 2: Create 3 div same class name "box" Access all and add unique text---
-
-let items = document.querySelectorAll(".box");
-let idx=1;
-for (item of items){
-    // console.log(item);
-    item.innerText =`New Unbique Item Box ${idx}`;
-    idx++;
-}
-
+// get attribute value 
+let para = document.querySelector("p");
+// console.log(para.getAttribute("class"))
+console.log(para.setAttribute("class","new"))
