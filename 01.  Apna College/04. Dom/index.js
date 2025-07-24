8df9d4a35338654130f16712a4b4b4596953895f
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-07-24 23:52:25 +0600

    Q2 solve Advance

--- 01.  Apna College/04. Dom/index.js
+++ 01.  Apna College/04. Dom/index.js
@@ -1,8 +1,10 @@
 // Qustion 2: Create 3 div same class name "box" Access all and add unique text---
 
-let divs = document.querySelectorAll(".box");
-// console.log(params);
-divs[0].innerText = "Unique Value Box 1";
-divs[1].innerText = "Unique Value Box 2";
-divs[2].innerText = "Unique Value Box 3";
+let items = document.querySelectorAll(".box");
+let idx=1;
+for (item of items){
+    // console.log(item);
+    item.innerText =`New Unbique Item Box ${idx}`;
+    idx++;
+}
 
