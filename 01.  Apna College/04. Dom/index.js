6d4070f1dcca43acaa1831e31ee6167f4ac4389a
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-07-24 23:19:34 +0600

    ** Different Between InnerText/InnerHtml **

--- 01.  Apna College/04. Dom/index.js
+++ 01.  Apna College/04. Dom/index.js
@@ -1,12 +1,3 @@
-// let heading =document.getElementById('heading');
-// console.dir(heading);
-// let btn = document.getElementsByClassName("btn-dom");
-// console.dir(btn)
-// console.log(btn)
-// let params = document.getElementsByTagName('p');
-// console.log(params);
-// console.dir(params);
-let firstelement = document.querySelector("p");
-console.dir(firstelement);
-let AllElement = document.querySelectorAll("p");
-console.dir(AllElement);
+let params = document.querySelector("div");
+console.log(params.innerText)
+console.log(params.innerHTML)
