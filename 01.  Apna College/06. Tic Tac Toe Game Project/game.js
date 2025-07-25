07c80cc28dd470adfcd13a3699c14b250036b0e0
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-07-25 21:46:45 +0600

    ** Players input clickable done **

--- 01.  Apna College/06. Tic Tac Toe Game Project/game.js
+++ 01.  Apna College/06. Tic Tac Toe Game Project/game.js
@@ -2,7 +2,6 @@ let boxs = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset");
 
 // Game Section -------------
-let turn0 = true; //2 Players (x/0)
 const winPattern = [
   [0, 1, 2],
   [3, 4, 5],
@@ -12,8 +11,18 @@ const winPattern = [
   [2, 5, 8],
   [2, 4, 6],
 ];
+
+let turn0 = true; //2 Players (x/0)
 boxs.forEach((box) => {
   box.addEventListener("click", () => {
     console.log("box Clicked");
+    if(turn0){
+      box.innerText = "0";
+      turn0 = false;
+    }else{
+      box.innerText ="X";
+      turn0 =true;
+    }
+    box.disabled =true;
   });
 });
