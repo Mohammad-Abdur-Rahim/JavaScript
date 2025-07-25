8a07c5cde46b378369ae6d9de3f2d7bef177c8a4
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-07-25 20:36:49 +0600

    Basic setup js file

---
+++ 01.  Apna College/06. Tic Tac Toe Game Project/game.js
@@ -0,0 +1,19 @@
+let boxs = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset");
+
+// Game Section -------------
+let turn0 = true; //2 Players (x/0)
+const winPattern = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+];
+boxs.forEach((box) => {
+  box.addEventListener("click", () => {
+    console.log("box Clicked");
+  });
+});
