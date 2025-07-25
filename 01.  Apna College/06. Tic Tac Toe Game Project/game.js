16c0c9adf743bfbad66a24aa8be725e84d5ddba6
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-07-25 22:00:32 +0600

    Value Show Array

--- 01.  Apna College/06. Tic Tac Toe Game Project/game.js
+++ 01.  Apna College/06. Tic Tac Toe Game Project/game.js
@@ -15,14 +15,27 @@ const winPattern = [
 let turn0 = true; //2 Players (x/0)
 boxs.forEach((box) => {
   box.addEventListener("click", () => {
-    console.log("box Clicked");
-    if(turn0){
+    // console.log("box Clicked");
+    if (turn0) {
       box.innerText = "0";
       turn0 = false;
-    }else{
-      box.innerText ="X";
-      turn0 =true;
+    } else {
+      box.innerText = "X";
+      turn0 = true;
     }
-    box.disabled =true;
+    box.disabled = true;
+    checkWinner();
   });
 });
+
+const checkWinner = () => {
+  for (pattern of winPattern) {
+    console.log(pattern[0], pattern[1], pattern[2]);
+    console.log(
+      boxs[pattern[0]].innerText,
+      boxs[pattern[1]].innerText,
+      boxs[pattern[2]].innerText
+    );
+    
+  }
+};
