e200c826d0374c5222cf0849bbc212679ff820de
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-07-25 06:40:36 +0600

    ** Complete Dom Chapter **

--- 01.  Apna College/04. Dom/index.js
+++
@@ -1,5 +0,0 @@
-// Q2: create p tag in html give class and style-- now create new class in css try to append this class 
-
-let test = document.querySelector("p");
-console.log(test);
-test.classList.add("newclass");
