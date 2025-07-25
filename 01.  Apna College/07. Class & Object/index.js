b401448ca248e3f763a0d60067e1a298a9900a39
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-07-26 00:08:04 +0600

    Class Blue print of Obj

--- 01.  Apna College/07. Class & Object/index.js
+++ 01.  Apna College/07. Class & Object/index.js
@@ -1,16 +1,9 @@
-const employee= {
-    name : "Rakib Khan",
-    age : 24,
-    isGood : true,
-    salary : 34000,
-    calTax(){
-        console.log("Tax rate is 10 %");
-    },
-
+class toyotaCar{
+    start(){
+        console.log("start car")
+    }
+     end(){
+        console.log("start end")
+    }
 };
-const  sakil = {
-    age: 22,
-    salary : 50000,
-};
-//Prototype set
-sakil.__proto__ =employee;
+let obj = new toyotaCar();
