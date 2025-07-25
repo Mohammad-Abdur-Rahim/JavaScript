4a30bf556c7054f6160640e8110ba3723fa0014a
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-07-25 08:02:23 +0600

    addEventListener(e,(callback))

--- 01.  Apna College/05. Events/index.js
+++ 01.  Apna College/05. Events/index.js
@@ -1,7 +1,17 @@
 let btn = document.querySelector("button");
-btn.onclick=(event)=>{
-    console.log(event);
-    console.log(event.type);
-    console.log(event.target);
-    console.log(event.clientX,event.clientY);
-}
+
+btn.addEventListener('click',()=>{
+    console.log("handler 1");
+});
+btn.addEventListener('click',()=>{
+    console.log("handler 2");
+});
+
+
+//best format usecase...................
+const handle3 =()=>{
+    console.log("handler 3");
+};
+
+btn.addEventListener('click',(handle3));
+btn.removeEventListener('click',handle3);
