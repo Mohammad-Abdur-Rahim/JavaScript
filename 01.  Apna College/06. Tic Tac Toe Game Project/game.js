dd05e771485bc179eeade31b693feab310fecf26
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-07-25 22:27:58 +0600

    complete without reset

--- 01.  Apna College/06. Tic Tac Toe Game Project/game.js
+++ 01.  Apna College/06. Tic Tac Toe Game Project/game.js
@@ -4,12 +4,12 @@ let resetBtn = document.querySelector("#reset");
 // Game Section -------------
 const winPattern = [
   [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
   [0, 3, 6],
+  [0, 4, 8],
   [1, 4, 7],
   [2, 5, 8],
-  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
 ];
 
 let turn0 = true; //2 Players (x/0)
@@ -28,14 +28,28 @@ boxs.forEach((box) => {
   });
 });
 
+
+const disableBoxs =()=>{
+  for(let box of boxs){
+    box.disabled=true;
+  }
+}
+
+let Message = document.querySelector("#msg");
+const showWinner=(winner)=>{
+  Message.innerText =`Winner is : ${winner}`;
+  disableBoxs();
+}
 const checkWinner = () => {
-  for (pattern of winPattern) {
-    console.log(pattern[0], pattern[1], pattern[2]);
-    console.log(
-      boxs[pattern[0]].innerText,
-      boxs[pattern[1]].innerText,
-      boxs[pattern[2]].innerText
-    );
-    
+  for (let pattern of winPattern) {
+    let pos1Val = boxs[pattern[0]].innerText;
+    let pos2Val = boxs[pattern[1]].innerText;
+    let pos3Val = boxs[pattern[2]].innerText;
+    if(pos1Val !="" && pos2Val !="" && pos3Val !=""){
+      if(pos1Val === pos2Val && pos2Val === pos3Val){
+        console.log("winner",pos1Val);
+        showWinner(pos1Val);
+      }
+    }
   }
 };
