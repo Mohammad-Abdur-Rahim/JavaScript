3837a1c54794d834e9605b1ea39274620a30b0f0
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-07-27 00:19:53 +0600

    Fetch Data Structure

--- 01.  Apna College/09. Api/app.js
+++ 01.  Apna College/09. Api/app.js
@@ -1,1 +1,7 @@
-console.log("test");
+const URL = "https://api.nationalize.io?name=nathaniel";
+
+const getData = async () => {
+  console.log("Getting Data....");
+  let response = await fetch(URL);
+  console.log(response.status);
+};
