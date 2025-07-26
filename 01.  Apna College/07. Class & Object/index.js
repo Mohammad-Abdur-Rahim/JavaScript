0506c4897a988165efa3eb7a5e08003e0eddef7c
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-07-26 09:59:51 +0600

    Class/ This value access

--- 01.  Apna College/07. Class & Object/index.js
+++ 01.  Apna College/07. Class & Object/index.js
@@ -1,9 +1,17 @@
-class toyotaCar{
-    start(){
-        console.log("start car")
-    }
-     end(){
-        console.log("start end")
-    }
-};
-let obj = new toyotaCar();
+class toyotaCar {
+  start() {
+    console.log("start car");
+  }
+
+  end() {
+    console.log("stop car");
+  }
+  setBrand(brand) {
+    this.brand = brand;
+  }
+}
+
+let car1 = new toyotaCar();
+car1.setBrand("Mode 1");
+let car2 = new toyotaCar();
+car2.setBrand("Mode 2");
