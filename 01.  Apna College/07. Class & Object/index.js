7e3031f623e6b03e7d752c3ce3d3880c109cae60
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-07-26 11:13:04 +0600

    ** Q1 Solve **

--- 01.  Apna College/07. Class & Object/index.js
+++ 01.  Apna College/07. Class & Object/index.js
@@ -1,32 +1,14 @@
-class Person{
-    constructor(name){
-        this.role ="Frontend React Developer";
-        this.name=name; 
+// Craete a college website . create class user with 2 properties nbame,email. it also called method viewData() that allow user view data .
+let data ="Secret Data";
+class User {
+    constructor(name,email){
+        this.name=name;
+        this.email=email;
     }
-    eat(){
-        console.log("Eating");
-    }
-    sleep(){
-        console.log("Sleep");
-    }
-    work(){
-        console.log("Nothing to work");
+    view(){
+        console.log("Website Data ",data);
     }
 }
-
-class Engineer extends Person{
-    constructor(name){
-        super(name); //invoke Parent Class Constructor
-    }
-    work(){
-        console.log("React Developer ");
-    }
-}
-class Doctor extends Person{
-    work(){
-        console.log("Treate the patient");
-    }
-}
-let personobj = new Person();
-let rahimobj = new Engineer("Md.Abdur Rahim");
-let shakibobj = new Doctor();
+let student1 = new User("Md. Abdur Rahim","Rahim44@gmail.com");
+let student2= new User(" Abdur ","asdfta@gmail.com");
+let teacher= new User("Dean","sakib6743t@gmail.com");
