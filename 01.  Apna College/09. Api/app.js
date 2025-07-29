20e953f9f865aa57f33be61af41484522d388e3b
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-07-29 21:58:33 +0600

    ** Api useable data basic **

--- 01.  Apna College/09. Api/app.js
+++ 01.  Apna College/09. Api/app.js
@@ -3,5 +3,7 @@ const URL = "https://api.nationalize.io?name=nathaniel";
 const getData = async () => {
   console.log("Getting Data....");
   let response = await fetch(URL);
-  console.log(response.status);
+  console.log(response);
+  let data = await response.json();
+  console.log(data);
 };
