f683278875bdb0ea026894244b86de0ea4d9fc30
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-07-29 22:16:26 +0600

    ** Currency converter App Js **

--- 01.  Apna College/09. Api/app.js
+++ 01.  Apna College/09. Api/app.js
@@ -1,9 +1,1 @@
-const URL = "https://api.nationalize.io?name=nathaniel";
-
-const getData = async () => {
-  console.log("Getting Data....");
-  let response = await fetch(URL);
-  console.log(response);
-  let data = await response.json();
-  console.log(data);
-};
+// MDN Document must Learn now start curency conrverter app 
