af871f21c1446051faa3f840280c09326a1caae3
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-08-05 11:29:39 +0600

    Api access pending

--- 01.  Apna College/09. Api/app.js
+++ 01.  Apna College/09. Api/app.js
@@ -1,1 +1,12 @@
-console.log("Done");
+const baseUrl = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+
+for(let select of dropdowns){
+    for(currCode in countryList){
+        let newOpt = document.createElement("option");
+        newOpt.innerText = currCode;
+        newOpt.value = currCode;
+        select.append(newOpt);
+    }
+}
